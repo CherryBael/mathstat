443edb580d85d988af3b493308f2824f4b781b7f
Arthur <Cherrybael@ChLaptop.localdomain>
2024-06-06 22:29:11 +0300

    добавлены более подробные комментарии к полям и методам класса multi_regression

--- lib/multi_regression.py
+++ lib/multi_regression.py
@@ -1,10 +1,11 @@
 # pandas и numpy
 import pandas as pd
 import numpy as np
-# функция рассчета критических значений
+# функции рассчета критических значений
 from lib.utils import f_critical_value as fcv, t_quant
-# считаем коэффициенты линейной регрессии
+
 class multi_regression:
+    # кэш для ленивой инициализации
     class cache:
         Cor_Var = None
         Cov = None
@@ -22,70 +23,71 @@ class multi_regression:
             exit(1)
         self.X = X
         self.Y = Y
-        self.regression_coefs()       # создает поле rgc -- коэфициенты линейной регрессии
+        self.regression_coefs()                     # создает поле rgc -- коэффициенты линейной регрессии
         self.df = len(self.Y) - len(self.X[0]) - 1  # количество степеней свободы
-        self.Y_pr = np.dot(self.X, self.rgc)
-        self.Y_mn = self.Y.mean()
-        self.e = self.Y - self.Y_pr
+        self.Y_pr = np.dot(self.X, self.rgc)        # Y, полученные из модели
+        self.Y_mn = self.Y.mean()                   # Y среднее
+        self.e = self.Y - self.Y_pr                 # вектор ошибок
+    # вычисление исправленной дисперсии
     @property
     def Cor_Var(self):
         if self.cache.Cor_Var is None: 
             self.cache.Cor_Var = np.dot(np.transpose(self.e),self.e)/self.df
         return self.cache.Cor_Var
+    # нахождение матрицы ковариации
     @property
     def Cov(self):
         if self.cache.Cov is None:
             self.cache.Cov = self.Cor_Var * np.linalg.pinv(np.dot(np.transpose(self.X),self.X))
         return self.cache.Cov
+    # нахождение вектора стандартных ошибок
     @property
     def SE(self):
         if self.cache.SE is None:
             self.cache.SE = [self.Cov[i][i] for i in range(len(self.Cov))]
         return self.cache.SE
+    # нахождение TSS
     @property
     def TSS(self):
         if self.cache.TSS is None:
             self.cache.TSS = sum([(y_i - self.Y_mn)**2 for y_i in self.Y])
         return self.cache.TSS
     @property
+    # нахождение ESS
     def ESS(self):
         if self.cache.ESS is None:
             self.cache.ESS = sum([(y_i - self.Y_mn)**2 for y_i in self.Y_pr])
         return self.cache.ESS
+    # нахождение RSS
     @property
     def RSS(self):
         if self.cache.RSS is None:
             self.cache.RSS = sum([(self.Y[i] - self.Y_pr[i])**2 for i in range(len(self.Y))])
         return self.cache.RSS
     @property
+    # нахождение коэффициента детерминации
     def R(self):
         if self.cache.R is None:
             self.cache.R = self.ESS/self.TSS
         return self.cache.R
-
-
-
-
-
-
-
-
+    # нахождение коэффициентов регрессии
     def regression_coefs(self):
         xtx = np.dot(np.transpose(self.X),self.X)
         xty = np.dot(np.transpose(self.X),self.Y)
         coefs = np.linalg.solve(xtx, xty)
         self.rgc = coefs
         return
+    # нахождение доверительных интервалов
     def trust_interval(self,i, alpha):
         return(self.rgc[i] - self.SE[i] * t_quant(self.df, alpha), self.rgc[i] + self.SE[i] * t_quant(self.df, alpha))
-
+    # F тест на значимость
     def F_test_for_significance(self, alpha):
         F = (self.R**2/(len(self.X[0]) - 1))/((1 - self.R**2)/(self.df + 1))
         #print("------------------------------------------")
         #print(F)
         #print(fcv(len(self.X[0]) - 1, self.df + 1, alpha))
         return True if F > fcv(len(self.X[0]) - 1, self.df + 1,alpha) else False
-
+    # F тест в более общем случае
     def F_test_for_variable(self, j):
         X_T = np.transpose(X)
         r_model = multi_regression(X_T[:j] + X_T[j+1:], Y)
--- lib/multi_regression_homework.py
+++ lib/multi_regression_homework.py
@@ -1,20 +1,16 @@
-import time
 from lib.multi_regression import multi_regression
-from lib.utils import prepare_data_for_heart_failure
-from lib.utils import is_ortogonal as iso
+from lib.utils import prepare_data_for_heart_failure, is_ortogonal as iso
 def multi_regression_homework():
     #X = np.array([[1,2,-1,3], [-1,1,0,0], [0,2,1,0], [0,0,1,-1],[1,-1,2,1]])
     #Y = np.transpose(np.array([1,2,3,4,5]))
     X, Y, Xnames, Yname = prepare_data_for_heart_failure()
-    alpha = 0.01 # для F тест
-    beta = 0.05  # для доверительных интервалов
+    alpha = 0.01       # для F теста
+    beta = 0.05        # для доверительных интервалов
     round_accuracy = 3 # количество знаков после запятой в выводе в stdout 
-    ts = time.time()
     rm = multi_regression(X, Y)
     for i in range(len(Xnames)):
         print(f"Коэффициент регрессии между {Yname} и {Xnames[i]} равен {round(rm.rgc[i], round_accuracy)}")
         tleft, tright = [round(x, round_accuracy) for x in rm.trust_interval(i, beta)]
         print(f"Принадлежит интервалу [{tleft},{tright}] с точностью {(1 - beta) * 100}%")
     print(f"Модель проходит F-тест на значимость с точностью {(1 - alpha) * 100}%" if rm.F_test_for_significance(alpha) else f"Модель не проходит F-тест на значимость с точностью {(1 - alpha) * 100}%")
-    #print(time.time() - ts)
     return
